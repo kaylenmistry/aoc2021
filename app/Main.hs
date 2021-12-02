ae55456c263797a0895b67df17397d1c41eb2050
Kaylen Mistry <kaylenmistry@gmail.com>
2021-12-02 16:34:58 +0000

    day 2 commit

--- app/Main.hs
+++ app/Main.hs
@@ -5,8 +5,38 @@ import System.IO
 
 main :: IO ()
 main = do 
-    contents <- readFile "data/day1-depths.txt"  
+    day1Main
+    day2Main
+
+-- Outputs
+
+day1Main :: IO ()
+day1Main = do 
+    contents <- readFile "data/day1.txt"  
+    print "Day 1 - Part 1:"
+    print . countDepthIncreases . map readInt . words $ contents
+    print "Day 1 - Part 2:"
     print . countDepthIncreases . generateSlidingWindow . map readInt . words $ contents
+
+day2Main :: IO ()
+day2Main = do 
+    contents <- readFile "data/day2.txt"  
+    let fileLines = lines contents
+    print "Day 2 - Part 1:"
+    let posOriginal = calculatePosition $ map readCommand fileLines
+    print posOriginal
+    print $ (\(x, d) -> x * d) posOriginal
+    print "Day 2 - Part 2:"
+    let posWithAim = calculatePositionWithAim $ map readCommand fileLines
+    print posWithAim
+    print $ (\(x, d, a) -> x * d) posWithAim
+
+-- Helpers
 
 readInt :: String -> Int
 readInt = read
+
+readCommand :: String -> Command
+readCommand ('f':'o':'r':'w':'a':'r':'d':' ': n) = Forward (readInt n)
+readCommand ('u':'p':' ': n) = Up (readInt n)
+readCommand ('d':'o':'w':'n':' ': n) = Down (readInt n)
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,4 +1,4 @@
-module Lib ( countDepthIncreases, generateSlidingWindow ) where
+module Lib ( countDepthIncreases, generateSlidingWindow, calculatePosition, calculatePositionResult, calculatePositionWithAim, Command(Forward, Up, Down)) where
 
 -- Day 1: AOC 2021
 
@@ -14,3 +14,26 @@ countDepthIncreases' (x1:x2:xs) n = countDepthIncreases' (x2:xs) count
 generateSlidingWindow :: Num a => [a] -> [a]
 generateSlidingWindow (x1:x2:x3:xs) = (x1 + x2 + x3) : (generateSlidingWindow (x2:x3:xs))
 generateSlidingWindow xs = []
+
+-- Day 2: AOC 2021
+
+data Command = Forward Int | Up Int | Down Int deriving (Show)
+
+calculatePositionResult :: (Int, Int) -> Int
+calculatePositionResult (x, y) = (x * y)
+
+calculatePosition :: [Command] -> (Int, Int)
+calculatePosition cs = foldl calculateNewPosition (0, 0) cs
+
+calculateNewPosition :: (Int, Int) -> Command -> (Int, Int)
+calculateNewPosition (x, d) (Forward n) = (x + n, d)
+calculateNewPosition (x, d) (Up n) = (x, d - n)
+calculateNewPosition (x, d) (Down n) = (x, d + n)
+
+calculatePositionWithAim :: [Command] -> (Int, Int, Int)
+calculatePositionWithAim cs = foldl calculateNewPositionWithAim (0, 0, 0) cs
+
+calculateNewPositionWithAim :: (Int, Int, Int) -> Command -> (Int, Int, Int)
+calculateNewPositionWithAim (x, d, aim) (Forward n) = (x + n, d + (aim * n), aim)
+calculateNewPositionWithAim (x, d, aim) (Up n) = (x, d, aim - n)
+calculateNewPositionWithAim (x, d, aim) (Down n) = (x, d, aim + n)
